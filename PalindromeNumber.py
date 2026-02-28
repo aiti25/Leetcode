42bce83bec3b53fabe8b283c823787a77ab76b00
aiti25 <baidyaaitijhya@gmail.com>
2026-02-28 16:19:25 +0530

    Palindrome Number

---
+++ PalindromeNumber.py
@@ -0,0 +1,12 @@
+# Palindrome Number
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        s = str(x)
+        
+        return s == s[::-1]
+        
